b7bb25c33b9a5ca0b2f498ed0b1cac4746db1be3
chianw <chiancw@gmail.com>
2022-12-20 15:11:12 +0800

    Fixed and final

--- azureday1/modules/virtual_network_peering/virtual_network_peering.tf
+++ azureday1/modules/virtual_network_peering/virtual_network_peering.tf
@@ -1,9 +1,8 @@
-
-module "peering" {
-  source                    = "./peering"
+resource "azurerm_virtual_network_peering" "peer" {
   for_each                  = var.peerings
   name                      = each.value.name
   resource_group_name       = each.value.resource_group_name
   virtual_network_name      = var.virtual_network_name
   remote_virtual_network_id = var.virtual_networks[each.key].id
+
 }
